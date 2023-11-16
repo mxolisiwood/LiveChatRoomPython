f6b0df2972e2d11bacf254ca0df4a4e57f0fd01c
Mxolisi <silabelamxolisi@gmail.com>
2023-11-16 03:17:53 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,78 @@
+
+from flask import Flask, render_template, request, session, redirect,url_for
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
+
+app = Flask(__name__)
+app.config["SECTRET_KEY"] = "Buya213Jesu"
+app.config['SECRET_KEY'] = 'super secret key'
+socketio = SocketIO(app)
+
+rooms = {
+
+}
+
+def generate_unique_code(Length):
+    while True:
+        code = ""
+        for _ in range(Length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break 
+
+
+#Creating two roots
+#1. Home page
+@app.route("/", methods=["POST", "GET"])
+def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter your name to continue.", code=code, name=name)
+        
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
+        
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist." ,code=code, name=name)
+
+        #storing information
+        session["room"] = room
+        session["name"] = name  
+        return redirect(url_for("room"))
+
+
+    return render_template("home.html")
+
+#2. Room page
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    
+
+    return render_template("room.html")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
